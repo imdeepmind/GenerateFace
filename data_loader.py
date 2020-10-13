bf48ea58754d75270ebdd50511b4bdb34c79bc8f
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-10-14 00:27:50 +0530

    updated the code to read image

--- data_loader.py
+++ data_loader.py
@@ -1,20 +1,20 @@
 from torch.utils.data import Dataset
-import cv2
+from PIL import Image
 import torch
 import os
 
 class FaceDataset(Dataset):
   def __init__(self, path, transform=None):
     self.__images = os.listdir(path)
+    self.__path = path
     self.__transform = transform
   
   def __len__(self):
     return len(self.__images)
   
   def __getitem__(self, index):
-    center = cv2.imread(self.__images[index], 1)
-
+    image = Image.open(os.path.join(self.__path, self.__images[index]))
     if self.__transform:
-      center = self.__transform(center)
+      image = self.__transform(image)
 
-    return center
+    return image
